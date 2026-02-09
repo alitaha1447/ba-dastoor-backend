1cabb22c8c34628fcddded4564f1a58cfaa68787
alitaha1447 <ali.taha1447@gmail.com>
2026-02-09 17:28:57 +0530

    set video quality in cloudinary

--- src/utils/videoUploadToCloudinary.js
+++ src/utils/videoUploadToCloudinary.js
@@ -4,8 +4,19 @@ const videoUploadToCloudinary = async (filePath, folder) => {
     return await cloudinary.uploader.upload(filePath, {
         folder,
         resource_type: "video", // ✅ FORCE VIDEO
-        eager: [],
-        eager_async: true
+        eager: [{
+            format: "mp4",
+            video_codec: "auto",
+            audio_codec: "aac",
+
+            bitrate: "auto",       // smart compression
+            quality: "auto",
+
+            width: 1280,          // prevent 4K waste
+            crop: "limit",
+        },],
+        eager_async: true,
+        fetch_format: "auto",
     });
 };
 
